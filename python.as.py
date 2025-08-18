d2957961d5b072854c6657651b53d14c3910ce9a
alyaa2004 <155041054+alyaa2004@users.noreply.github.com>
2025-08-18 20:55:57 +0300

    Add files via upload

---
+++ python.as.py
@@ -0,0 +1,24 @@
+
+my_list = []
+
+
+my_list.append(100)
+my_list.append(200)
+my_list.append(300)
+my_list.append(400)
+
+my_list.insert(10, 150)
+
+
+my_list.extend([500, 600, 700])
+
+
+my_list.pop()
+
+
+my_list.sort()
+
+
+index_30 = my_list.index(300)
+print("My List:", my_list)
+print("Index of 30:", index_30)
